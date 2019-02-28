e5a75f3e8eb7b10b96c4ec59b39e362eb74d8d7d
Andrey Ogurtsov <ogurtsov.a.b@gmail.com>
2019-02-28 19:15:44 +0200

    test commit

--- R/metrics.R
+++ R/metrics.R
@@ -2,6 +2,7 @@ rmse <- function(ground_truth, prediction) {
     sqrt(mean((ground_truth - prediction)^2))
 }
 
+# 
 mae <- function(ground_truth, prediction) {
     mean(abs(ground_truth - prediction))
 }
